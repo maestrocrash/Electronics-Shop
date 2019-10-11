84d1854b1f96a95e40ece89171a71cf109192970
maestrocrash <maestrocrash@gmail.com>
2019-10-11 16:20:54 +0300

    Initial Commit

---
+++ Electronics Shop/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Electronics Shop
+//
+//  Created by Shakhvorostov on 11.10.2019.
+//  Copyright © 2019 Shakhvorostov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
